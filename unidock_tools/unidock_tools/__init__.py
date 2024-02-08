1d237e3fa2916459ffcd715a9751795762199e30
dp-yuanyn <yuanyn@dp.tech>
2024-02-08 16:36:59 +0800

    build:use toml

--- unidock_tools/unidock_tools/__init__.py
+++ unidock_tools/unidock_tools/__init__.py
@@ -1,3 +1,10 @@
 from unidock_tools.utils import init_logging
 
 init_logging()
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("binary4fun")
+except PackageNotFoundError:
+    __version__ = "unknown"
