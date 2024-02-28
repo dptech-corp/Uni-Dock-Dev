f851a0d7c80d41480bfa7cfcc8c247f644057352
ysy <ysyecust@gmail.com>
2024-02-28 15:36:21 +0800

    Fix unit test

--- unidock/unittest/ParseTests/parse_pdqt_test.cpp
+++ unidock/unittest/ParseTests/parse_pdqt_test.cpp
@@ -1,18 +1,13 @@
-#include "../catch_amalgamated.hpp"
+#include <catch2/catch_test_macros.hpp>
 #include "parse_pdbqt.h"
 #include "atom.h"
-// #include "parse_pdbqt.cpp"
+
 
 TEST_CASE("parse pdbqt rigid", "[parse_pdbqt_rigid]") {
-    // 假设你有一个有效的PDBQT文件路径
+    // Test pdbqt atom number
     std::string valid_pdbqt_receptor_path = "../unitest/ParseTests/def.pdbqt";
     rigid r;
     REQUIRE_NOTHROW(parse_pdbqt_rigid(valid_pdbqt_receptor_path, r));
-    REQUIRE(r.atoms.size() == 1613); // 假设你的atom解析函数会添加到atoms向量
-    // // 测试正常情况
-    // // REQUIRE_NOTHROW(parse_receptor_pdbqt(valid_pdbqt_receptor_path));
-    // // atom_type::t atype;
-    // // pdbqt_initializer tmp(atype);
-    // model m = parse_receptor_pdbqt(valid_pdbqt_receptor_path);
-    // std::cout <<"aaaaaaaaaa"<<m.num_atoms() <<std::endl; 
+    REQUIRE(r.atoms.size() == 1613);
+
 }
